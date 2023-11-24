ed594427e789b26cfc08bea24bbbde9415e754a2
Joe Monkila <jnkiwa25@gmail.com>
2023-11-24 08:04:01 +0100

    create models

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to api" });
 });
