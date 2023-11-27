1cc70c505eba75e00e2530929460e5491fe9bbbe
Joe Monkila <jnkiwa25@gmail.com>
2023-11-27 17:17:39 +0100

    routes for student

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get("/", (req, res) => {
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 require('./routes/enseignant.routes')(app);
+require('./routes/eleve.routes')(app);
 
 
 const PORT = process.env.PORT || 8080;
