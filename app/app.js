edf483ea7eb05621d171ef48e2a6be2ac4d67335
Joe Monkila <jnkiwa25@gmail.com>
2023-11-29 11:17:06 +0100

    changed directory

--- app/app.js
+++ app/app.js
@@ -28,6 +28,7 @@ require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 require('./routes/enseignant.routes')(app);
 require('./routes/eleve.routes')(app);
+require('./routes/personnel.routes')(app);
 
 
 const PORT = process.env.PORT || 8080;
