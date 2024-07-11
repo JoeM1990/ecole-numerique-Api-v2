b1f848e48ea7aca4629aad9589aa4663ea1bfbbe
Joe Monkila <70669743+JoeM1990@users.noreply.github.com>
2024-07-11 09:10:01 +0100

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "monkila1234",
-    DB: "ecolenumerique",
+    PASSWORD: "",
+    DB: "",
     dialect: "mysql",
     pool: {
       max: 5,
